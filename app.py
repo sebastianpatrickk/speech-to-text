249e568321783d2bc4a78a20deb528cf39a75c1f
Sebastian Patrick Klen <sebastianpatrickklen@gmail.com>
2024-01-12 16:31:29 +0000

    Add voiceover transcription functionality

--- app.py
+++ app.py
@@ -1,11 +1,5 @@
-#-----------------------------------------------------------------------------------------
-# Copyright (c) Microsoft Corporation. All rights reserved.
-# Licensed under the MIT License. See LICENSE in the project root for license information.
-#-----------------------------------------------------------------------------------------
+import whisper
 
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-    return app.send_static_file("index.html")
+model = whisper.load_model("base")
+print(model)
+result = model.transcribe("original-voiceover.wav")
